97ee72713aaa9b9563b1deacc901283d1a767dc1
Nadira Seitkazy <nadiraseitkazu@gmail.com>
2025-05-20 10:09:04 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:jihc_coin/firebase_options.dart';
 import 'package:jihc_coin/providers/user_provider.dart';
 import 'package:jihc_coin/screens/first_page.dart';
 import 'package:provider/provider.dart';
 
-void main(List<String> args) {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
     MultiProvider(
       providers: [ChangeNotifierProvider(create: (_) => UserProvider())],
